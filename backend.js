d0248e442915cd2e2cf866caaba7f97a3da0811d
Groophy <groophylifefor@gmail.com>
2023-06-08 15:42:52 +0300

    directory not exits fix

--- backend.js
+++ backend.js
@@ -343,7 +343,13 @@ function setupRoutes() {
     let html = json2html_template_standart(JSON.parse(decodeURIComponent(req.query.json)))
 
     let uuid = uuidv4();
-    fs.readdirSync(path.join(__dirname, 'private', 'temp_previews')).forEach(file => fs.unlinkSync(path.join(__dirname, 'private', 'temp_previews', file)));
+    if (!fs.existsSync('./private/temp_previews/')) {
+      fs.mkdirSync(directoryPath, { recursive: true });
+    }
+    try
+    {
+      fs.readdirSync(path.join(__dirname, 'private', 'temp_previews')).forEach(file => fs.unlinkSync(path.join(__dirname, 'private', 'temp_previews', file)));
+    } catch { /* IGNORE */}
     const filePath = __dirname + '/private/temp_previews/' + uuid + '.pdf';
 
     getBrowserInstance()
